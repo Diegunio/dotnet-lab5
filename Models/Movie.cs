567b2a5f08cdc9e9d3d1e8504b6187d5a884b57c
Diego Lavayen <diego@example.com>
2025-10-19 16:29:13 +0200

    5.5 - map frontend

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace lab5.Models;
 
 public class Movie
 {
     public int Id { get; set; }
     public string Title { get; set; } = "";
+    [UIHint("LongText")]
     public string Description { get; set; } = "";
     public int Rating { get; set; }
     public string TrailerLink { get; set; } = "";
